9ded77db631ec86d8ff9a44e2d5dfa23ac328175
leomuehlfeld <leo.muehlfeld@icloud.com>
2019-03-14 13:10:40 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import styled from 'styled-components';
+
+const Headline = styled.h1`
+  color: blue;
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Air Print</h1>
+      <Headline>Air Print</Headline>
       <form>
         <input type="text"/>
         <button>Print</button>
