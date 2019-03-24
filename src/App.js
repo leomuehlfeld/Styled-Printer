b68acbad9205ea25306cf22892627c787ff93ccf
leomuehlfeld <leo.muehlfeld@icloud.com>
2019-03-25 00:51:31 +0100

    Init Headline Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 
-const Headline = styled.h1`
-  color: blue;
-`
+//import Typo
+import Headline from './components/typo/headline';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Headline>Air Print</Headline>
+      <Headline>Printer</Headline>
       <form>
         <input type="text"/>
         <button>Print</button>
--- src/components/typo/headline.js
+++ src/components/typo/headline.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 const Headline = styled.h1`
-  font-size: 1em;
-  color: black;
-  font-weight: medium
+  font-size: 2em;
+  color: blue;
+  font-weight: medium;
 `
 
-export default Headline
+export default Headline;
