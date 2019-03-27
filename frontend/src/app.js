6e8ee8f3eb43710a9a145a7cb883c1cb79f5223c
leomuehlfeld <leo.muehlfeld@icloud.com>
2019-03-27 23:04:09 +0100

    Timo Magic

---
+++ frontend/src/app.js
@@ -0,0 +1,82 @@
+import React, { Component, useState, useEffect, useRef } from "react";
+
+// Import Typo
+import Headline from "./components/typo/headline";
+
+const useSocket = (url, handleConnection, handleMessage) => {
+  const ref = useRef();
+
+  useEffect(
+    () => {
+      let ws = ref.current;
+      if (!ws) {
+        ws = new WebSocket(url);
+        ref.current = ws;
+      }
+
+      ws.onopen = handleConnection;
+      ws.onmessage = handleMessage;
+
+      return () => {
+        ws.close();
+      };
+    },
+    [url]
+  );
+
+  return ref && ref.current;
+};
+
+const App = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const socket = useSocket(
+    "ws://192.168.0.108:8080",
+    () => {
+      console.log("Connected");
+    },
+    messageEvent => {
+      const newMessages = JSON.parse(messageEvent.data);
+      setMessages(newMessages);
+    }
+  );
+
+  console.log("socket", socket);
+  return (
+    <div className="App">
+      <Headline>Printer</Headline>
+      <form>
+        <input
+          onChange={e => setMessage(e.target.value)}
+          value={message}
+          type="text"
+        />
+        <button
+          onClick={e => {
+            e.preventDefault();
+            setMessages([
+              ...messages,
+              {
+                message,
+                date: new Date()
+              }
+            ]);
+            socket.send(message);
+          }}
+        >
+          Print {message}
+        </button>
+      </form>
+      <ul>
+        {messages.map(m => (
+          <li>
+            {m.message} {new Date(m.date).toLocaleDateString()}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
--- frontend/src/components/typo/headline.js
+++ frontend/src/components/typo/headline.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Headline = styled.h1`
   font-size: 2em;
   color: blue;
   font-weight: medium;
-`
+`;
 
 export default Headline;
