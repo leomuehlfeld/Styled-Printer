0c342780d0cbc891a87d5a20cc60c263cf384100
leomuehlfeld <leo.muehlfeld@icloud.com>
2019-03-28 00:30:00 +0100

    Rem component import

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 // Import Typo
 import Headline from "./components/typo/headline";
