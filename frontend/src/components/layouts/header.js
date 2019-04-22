ca9c326e69e7eb61e50e183856f41edd230e5c7f
leomuehlfeld <leo.muehlfeld@icloud.com>
2019-04-23 01:19:33 +0200

    Refine header

--- frontend/src/components/layouts/header.js
+++ frontend/src/components/layouts/header.js
@@ -1,15 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 import Logo from "../icons/logo.js";
-import Headline from "../typo/headline.js";
 
 const Wrapper = styled.div`
   position: fixed;
+  max-width: 250px;
+  margin: 20px;
+`;
+
+const Headline = styled.span`
+  font-weight: 600;
+  display: inline-block;
+  margin-top: 18px;
+  margin-left: 20px;
+`;
+
+const Link = styled.a`
+  color: black;
+  text-decoration: none;
+  display: inline-block;
+  margin-left: 20px;
+
+  &:hover {
+    color: #ffcc32;
+  }
 `;
 
 export default () => (
   <Wrapper>
     <Logo />
     <Headline>Air Printer</Headline>
+    <Link href="https://github.com/leomuehlfeld" target="blank">
+      View on Github
+    </Link>
   </Wrapper>
 );
--- frontend/src/components/typo/headline.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Headline = styled.h1`
-  font-size: 2em;
-  font-weight: medium;
-`;
-
-export default Headline;
